51bbaa0b4a6174462758458ebce07a0db71c232e
EPetrineLynghaug <136695793+EPetrineLynghaug@users.noreply.github.com>
2024-04-29 21:29:23 +0200

    html kort for login og registrere bruker er klar til å omgjøres i js.

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@
 import Navbar from './navbar.component.js';
 import Carousel from './carousel.component.js';
 import Card from './card.component.js';
+import login from './login.component.js'
 
 let baseUrl = "https://v2.api.noroff.dev/";
 let postContainer = document.querySelector(".post-container");
