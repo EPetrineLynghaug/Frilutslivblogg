63d009913a1351fb9037eb23b35765442709018b
EPetrineLynghaug <136695793+EPetrineLynghaug@users.noreply.github.com>
2024-05-20 15:38:27 +0200

    footer update.

--- js/components/footer.component.js
+++ js/components/footer.component.js
@@ -14,20 +14,23 @@ class Footer extends HTMLElement {
             <img src="/media/Eventyrer.PNG" alt="Brandinglogo">
             <div class="footer-logo-shadow"></div>
           </div>
-            <b>Petrine Lynghaug</b>
-            <span class="copy-right">Copyright © 2024 - All right reserved</span>
         </div>
-        <ul class="footer-bottom">
-            <li>
-                <a href="/index.html">Hjem</a>
-            </li>
-            <li>
-                <a href="/about.html">Om meg</a>
-            </li>
-            <li>
-                <a href="/account/login.html">Login</a>
-            </li>
-        </ul>
+
+        <div class="footer-bottom">
+          <span class="name">Petrine Lynghaug</span>
+          <ul class="footer-ul">
+              <li>
+                  <a href="/index.html">Hjem</a>
+              </li>
+              <li>
+                  <a href="/about.html">Om meg</a>
+              </li>
+              <li>
+                  <a href="/account/login.html">Login</a>
+              </li>
+          </ul>
+          <span class="copy-right">Copyright © 2024 - All right reserved</span>
+        </div>
     `;
   }
 }
