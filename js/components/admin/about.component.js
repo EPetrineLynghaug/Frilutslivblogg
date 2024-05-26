5ce0cfd3d6d038914540b257caa4dd0dbb198512
EPetrineLynghaug <136695793+EPetrineLynghaug@users.noreply.github.com>
2024-05-26 19:34:16 +0200

    .

--- js/components/admin/about.component.js
+++ js/components/admin/about.component.js
@@ -1,16 +1,14 @@
-
 class About extends HTMLElement {
- constructor(){
-  super();
+  constructor() {
+    super();
+  }
 
- }
+  connectedCallback() {
+    this.render();
+  }
 
- connectedCallback(){
-  this.render();
- }
-
- render() {
-this.innerHTML = `
+  render() {
+    this.innerHTML = `
     <div class="about-page-img">
         <img src="./media/komprimertgithubbilde.jpg" alt="meg">
     </div>
@@ -19,16 +17,15 @@ this.innerHTML = `
         <div class="about-page-text">
             <h1>Om meg</h1>
             <p>
-                Hei! Jeg heter Petrine Lyngghaug, og sammen med min trofaste følgesvenn, hunden Suzie,
-                deler jeg våre spennende eventyr her på bloggen. Når jeg ikke er ute på nye oppdagelser, 
-                studerer jeg utvikling på heltid. Jeg håper du finner inspirasjon til ditt neste eventyr 
-                gjennom våre historier!
+            Hei! 
+            Jeg heter Petrine Lynghaug. Sammen med min trofaste, firebeinte følgesvenn Suzie deler jeg våre spennende eventyr her på bloggen. Når jeg ikke er ute på nye oppdagelser studerer jeg nettside-utvikling på heltid.
+            Jeg håper du finner inspirasjon til ditt neste eventyr gjennom våre historier!
             </p>
             <a href="/index.html" class="btn btn-back" id="forsiden">Tilbake til forsiden</a>
         </div> 
         <img src="./media/suzie.png" alt="suzie, tegnet av meg">
     </div>
 `;
-}
+  }
 }
 export default About;
